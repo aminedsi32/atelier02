89584ce51730f821c4a239ece5e37c97e92e8ed7
aminedsi32 <93100993+aminedsi32@users.noreply.github.com>
2021-10-24 23:03:56 +0100

    Add files via upload

---
+++ voitures/src/main/java/com/jen/voitures/entities/Personne.java
@@ -0,0 +1,48 @@
+package com.jen.voitures.entities;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Personne {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long idPersonne;
+private String nomPersonne;
+private String prePersonne;
+@OneToMany (mappedBy="personne")
+private List<Voiture> voitures;
+public Personne() {
+	super();
+	
+}
+public Long getIdPersonne() {
+	return idPersonne;
+}
+public void setIdPersonne(Long idPersonne) {
+	this.idPersonne = idPersonne;
+}
+public String getNomPersonne() {
+	return nomPersonne;
+}
+public void setNomPersonne(String nomPersonne) {
+	this.nomPersonne = nomPersonne;
+}
+public String getPrePersonne() {
+	return prePersonne;
+}
+public void setPrePersonne(String prePersonne) {
+	this.prePersonne = prePersonne;
+}
+}
